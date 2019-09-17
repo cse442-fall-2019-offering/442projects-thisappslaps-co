9cf73eaa2a9d5cfe613224d4bd9ce86506eb9f19
Garrett <gsilver@buffalo.edu>
2019-09-17 16:26:00 -0400

    Created SceneForm Fragment
    Created the sceneform fragment. This opens up the camera in AR mode. Also changed the min SDK to 24 to allow the APIs to work properly. As well as the AndroidManifest needed permisions for AR and the Camera.

--- app/src/main/java/com/example/a442projects_thisappslaps_co/MainActivity.java
+++ app/src/main/java/com/example/a442projects_thisappslaps_co/MainActivity.java
@@ -1,22 +1,51 @@
 package com.example.a442projects_thisappslaps_co;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int MY_CAMERA_PERMISSIONS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        requestPermissionForCamera();
+    }
+
+    private void requestPermissionForCamera() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.CAMERA},
+                    MY_CAMERA_PERMISSIONS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (MY_CAMERA_PERMISSIONS == requestCode) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+            }
+        }
     }
 }
