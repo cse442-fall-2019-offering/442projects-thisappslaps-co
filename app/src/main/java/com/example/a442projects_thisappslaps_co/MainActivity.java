f5bd1f77a10167b667c0eed0031d58a000947f4e
Anthony I <theintrone@gmail.com>
2019-09-21 13:35:37 -0400

    Launch Gallery UI From Gallery Image Button
    Created the GalleryFragment class that uses the gallery_fragment.xml layout file. Created the GalleryController that creates a dummyList tp populate the Gallery RecyclerView. Also added resouces to the res/string file and the res/color file. Added the startFragment() method in the MainActivity to start Fragments from now on.

--- app/src/main/java/com/example/a442projects_thisappslaps_co/MainActivity.java
+++ app/src/main/java/com/example/a442projects_thisappslaps_co/MainActivity.java
@@ -4,19 +4,38 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.a442projects_thisappslaps_co.Gallery.GalleryFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static int MY_CAMERA_PERMISSIONS;
+
+    private ImageButton mGalleryImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViewVariable();
+        setListeners();
+    }
+
+    private void initializeViewVariable() {
+        mGalleryImageButton = findViewById(R.id.gallery_image_btn);
+    }
+
+    private void setListeners() {
+        mGalleryImageButton.setOnClickListener(this);
     }
 
     @Override
@@ -47,5 +66,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.gallery_image_btn:
+                startFragment(new GalleryFragment(), true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void startFragment(Fragment fragment, boolean shouldAddToBackStack) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        if (shouldAddToBackStack) {
+            fragmentTransaction.addToBackStack(null);
+        }
+        fragmentTransaction.commit();
     }
 }
