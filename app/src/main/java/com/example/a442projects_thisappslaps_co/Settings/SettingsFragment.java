dc1c355ed62ee3dea7d27af73cb5434bbe17fed2
Hannah Wlasowicz <hannahwl@buffalo.edu>
2019-10-13 18:25:46 -0400

    created initial event handlers for the switch components

--- app/src/main/java/com/example/a442projects_thisappslaps_co/Settings/SettingsFragment.java
+++ app/src/main/java/com/example/a442projects_thisappslaps_co/Settings/SettingsFragment.java
@@ -5,12 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
+
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.a442projects_thisappslaps_co.R;
 
 public class SettingsFragment extends Fragment implements View.OnClickListener {
+    SwitchCompat switch1;
+    SwitchCompat switch2;
+    SwitchCompat switch3;
+    SwitchCompat switch4;
+    SwitchCompat switch5;
 
     public SettingsFragment() { }
 
@@ -25,6 +34,68 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
         ImageButton backImageButton = view.findViewById(R.id.back_image_btn);
         backImageButton.setOnClickListener(this);
 
+        switch1 = view.findViewById(R.id.switch_1_switch_compat);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                }
+                else {
+
+                }
+            }
+        });
+
+        switch2 = view.findViewById(R.id.switch_2_switch_compat);
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+
+                }
+                else {
+
+                }
+            }
+        });
+        switch3 = view.findViewById(R.id.switch_3_switch_compat);
+        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+
+                }
+                else {
+
+                }
+            }
+        });
+
+        switch4 = view.findViewById(R.id.switch_4_switch_compat);
+        switch4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+
+                }
+                else {
+
+                }
+            }
+        });
+        switch5 = view.findViewById(R.id.switch_5_switch_compat);
+        switch5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+
+                }
+                else {
+
+                }
+            }
+        });
+
         return view;
     }
     
@@ -35,4 +106,5 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
             getFragmentManager().popBackStackImmediate();
         }
     }
+
 }
