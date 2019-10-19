1e4cc975bee4803ef2044188fc44eab85439fbe8
Hannah Wlasowicz <hannahwl@buffalo.edu>
2019-10-19 11:56:46 -0400

    Added SharedPreferences to the event handlers to allow switch values to be saved in any state the app may be in.

--- app/src/main/java/com/example/a442projects_thisappslaps_co/Settings/SettingsFragment.java
+++ app/src/main/java/com/example/a442projects_thisappslaps_co/Settings/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.a442projects_thisappslaps_co.Settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,9 +18,13 @@ import com.example.a442projects_thisappslaps_co.R;
 public class SettingsFragment extends Fragment implements View.OnClickListener {
     SwitchCompat switch1;
     SwitchCompat switch2;
-    SwitchCompat switch3;
-    SwitchCompat switch4;
-    SwitchCompat switch5;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String SWITCH1 = "switch1";
+    public static final String SWITCH2 = "switch2";
+
+    private boolean switch1_checked_value;
+    private boolean switch2_checked_value;
 
     public SettingsFragment() { }
 
@@ -34,68 +39,27 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
         ImageButton backImageButton = view.findViewById(R.id.back_image_btn);
         backImageButton.setOnClickListener(this);
 
+
         switch1 = view.findViewById(R.id.switch_1_switch_compat);
+        switch2 = view.findViewById(R.id.switch_2_switch_compat);
+        loadData();
+        updateViews();
+
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                }
-                else {
-
-                }
+                saveData();
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "Switch1 has Changed", Toast.LENGTH_LONG).show();
             }
         });
 
-        switch2 = view.findViewById(R.id.switch_2_switch_compat);
         switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-
-                }
-                else {
-
-                }
+                saveData();
             }
         });
-        switch3 = view.findViewById(R.id.switch_3_switch_compat);
-        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-
-                }
-                else {
-
-                }
-            }
-        });
-
-        switch4 = view.findViewById(R.id.switch_4_switch_compat);
-        switch4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-
-                }
-                else {
-
-                }
-            }
-        });
-        switch5 = view.findViewById(R.id.switch_5_switch_compat);
-        switch5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-
-                }
-                else {
-
-                }
-            }
-        });
-
         return view;
     }
     
@@ -105,6 +69,27 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
             assert getFragmentManager() != null;
             getFragmentManager().popBackStackImmediate();
         }
+    }
+
+    public void saveData() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putBoolean(SWITCH1, switch1.isChecked());
+        editor.putBoolean(SWITCH2, switch2.isChecked());
+
+        editor.apply();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, 0);
+        switch1_checked_value = sharedPreferences.getBoolean(SWITCH1, false);
+        switch2_checked_value = sharedPreferences.getBoolean(SWITCH2, false);
+    }
+
+    public void updateViews(){
+        switch1.setChecked(switch1_checked_value);
+        switch2.setChecked(switch2_checked_value);
     }
 
 }
