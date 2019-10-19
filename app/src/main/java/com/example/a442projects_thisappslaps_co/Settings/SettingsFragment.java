72911176f5d32a26b5634221f45421a329da786f
Hannah Wlasowicz <hannahwl@buffalo.edu>
2019-10-19 15:04:20 -0400

    Added a change password field that when the login page is created, change the user's password. The field has been hashed out for privacy/security reasons.

--- app/src/main/java/com/example/a442projects_thisappslaps_co/Settings/SettingsFragment.java
+++ app/src/main/java/com/example/a442projects_thisappslaps_co/Settings/SettingsFragment.java
@@ -10,7 +10,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ImageButton;
 import android.widget.CompoundButton;
-import android.widget.Toast;
 
 
 import androidx.appcompat.widget.SwitchCompat;
@@ -19,24 +18,25 @@ import androidx.fragment.app.Fragment;
 import com.example.a442projects_thisappslaps_co.R;
 
 public class SettingsFragment extends Fragment implements View.OnClickListener {
-    SwitchCompat switch1;
-    SwitchCompat switch2;
+    SwitchCompat switch3;
     SwitchCompat switch4;
     SwitchCompat switch5;
     TextView username_text_view;
-    Button saveButton;
     EditText username_edit_text;
+    Button username_saveButton;
+    EditText password_edit_text;
+    Button password_saveButton;
 
     public static final String SHARED_PREFS = "sharedPrefs";
-    public static final String SWITCH1 = "switch1";
-    public static final String SWITCH2 = "switch2";
-    public static final String USERNAME = "username1";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String SWITCH3 = "switch3";
     public static final String SWITCH4 = "switch4";
     public static final String SWITCH5 = "switch5";
 
-    private boolean switch1_checked_value;
-    private boolean switch2_checked_value;
     private String  username_value;
+    private String  password_value;
+    private boolean switch3_checked_value;
     private boolean switch4_checked_value;
     private boolean switch5_checked_value;
 
@@ -54,37 +54,28 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
         backImageButton.setOnClickListener(this);
 
 
-        switch1 = view.findViewById(R.id.switch_2_switch_compat);
-        switch2 = view.findViewById(R.id.switch_3_switch_compat);
+        switch3 = view.findViewById(R.id.switch_3_switch_compat);
         switch4 = view.findViewById(R.id.switch_4_switch_compat);
         switch5 = view.findViewById(R.id.switch_5_switch_compat);
-        saveButton = view.findViewById(R.id.user_name_save_button);
+
+        username_saveButton = view.findViewById(R.id.user_name_save_button);
         username_text_view = view.findViewById(R.id.user_name_text_view);
         username_edit_text = view.findViewById(R.id.user_name_edit_text);
 
-        saveButton.setOnClickListener(new View.OnClickListener() {
+        password_edit_text = view.findViewById(R.id.password_edit_text);
+
+        username_saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 username_text_view.setText(username_edit_text.getText().toString());
                 saveData();
-                Toast.makeText(getActivity().getApplicationContext(),
-                        "Updated ", Toast.LENGTH_LONG).show();
             }
         });
 
         loadData();
         updateViews();
 
-        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                saveData();
-//                Toast.makeText(getActivity().getApplicationContext(),
-//                        "Switch1 has Changed", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 saveData();
@@ -95,8 +86,6 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 saveData();
-//                Toast.makeText(getActivity().getApplicationContext(),
-//                        "Switch1 has Changed", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -104,12 +93,8 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 saveData();
-//                Toast.makeText(getActivity().getApplicationContext(),
-//                        "Switch1 has Changed", Toast.LENGTH_LONG).show();
             }
         });
-
-
 
         return view;
     }
@@ -126,9 +111,9 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, 0);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        editor.putBoolean(SWITCH1, switch1.isChecked());
-        editor.putBoolean(SWITCH2, switch2.isChecked());
         editor.putString(USERNAME, username_edit_text.getText().toString());
+        editor.putString(PASSWORD, password_edit_text.getText().toString());
+        editor.putBoolean(SWITCH3, switch3.isChecked());
         editor.putBoolean(SWITCH4, switch4.isChecked());
         editor.putBoolean(SWITCH5, switch5.isChecked());
 
@@ -137,20 +122,16 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
 
     public void loadData() {
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, 0);
-        switch1_checked_value = sharedPreferences.getBoolean(SWITCH1, false);
-        switch2_checked_value = sharedPreferences.getBoolean(SWITCH2, false);
-
         username_value = sharedPreferences.getString(USERNAME, "User Name");
-        Toast.makeText(getActivity().getApplicationContext(),
-                "Updated to: " + username_value , Toast.LENGTH_LONG).show();
+        password_value = sharedPreferences.getString(PASSWORD, "");
+        switch3_checked_value = sharedPreferences.getBoolean(SWITCH3, false);
         switch4_checked_value = sharedPreferences.getBoolean(SWITCH4, false);
         switch5_checked_value = sharedPreferences.getBoolean(SWITCH5, false);
 
     }
 
     public void updateViews(){
-        switch1.setChecked(switch1_checked_value);
-        switch2.setChecked(switch2_checked_value);
+        switch3.setChecked(switch3_checked_value);
         switch4.setChecked(switch4_checked_value);
         switch5.setChecked(switch5_checked_value);
         username_text_view.setText(username_value);
