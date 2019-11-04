150d68152d5248359b1c8f8dcf3c254ff9ce0d81
Garrett <gsilver@buffalo.edu>
2019-11-04 11:42:42 -0500

    Created the ability to take a picture, and then store that to a local database

---
+++ app/src/main/java/com/example/a442projects_thisappslaps_co/Gallery/Database/DatabaseCursorWrapper.java
@@ -0,0 +1,4 @@
+package com.example.a442projects_thisappslaps_co.Gallery.Database;
+
+public class DatabaseCursorWrapper {
+}
---
+++ app/src/main/java/com/example/a442projects_thisappslaps_co/Gallery/Project.java
@@ -0,0 +1,7 @@
+package com.example.a442projects_thisappslaps_co.Gallery;
+
+public class Gallery {
+
+    private String mUri;
+    private 
+}
